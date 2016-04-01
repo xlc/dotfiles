bdc5aa8415d8417525a39572c9acc243c123f9ff
Xiliang Chen <xlchen1291@gmail.com>
2016-04-01 15:52:45 +1300

    add scripts

---
+++ release_keyboard.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+blueutil power 0
+
+ssh xiliangchen@xiliangchen-imac.local '/usr/local/bin/blueutil power 1'
---
+++ steal_keyboard.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+blueutil power 1
+
+ssh xiliangchen@xiliangchen-imac.local '/usr/local/bin/blueutil power 0'
